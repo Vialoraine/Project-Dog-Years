ec6b143842015ca00256699f24c8b8da3d04298e
Loreina Salalila <vialoraine.gg@gmail.com>
2024-10-15 01:27:39 -0700

    task 4 completed

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 //My current age
 const myAge = 29;
 
-//Initalizing earlyYears to have a value of 2
+//Initializing earlyYears to have a value of 2
 let earlyYears = 2;
 earlyYears = earlyYears * 10.5;
+
+//Initializing laterYears to have a value of myAge - 2
+let laterYears = myAge - 2;
