6a5e99e36d689ef97c003fead7ed159e499d7727
Loreina Salalila <vialoraine.gg@gmail.com>
2024-10-15 01:36:29 -0700

    task 10 completed

--- app.js
+++ app.js
@@ -3,18 +3,22 @@ const myAge = 29;
 
 //Initializing earlyYears to have a value of 2
 let earlyYears = 2;
-earlyYears = earlyYears * 10.5;
+// earlyYears = earlyYears * 10.5;
+earlyYears *= 10.5;
 
 //Initializing laterYears to have a value of myAge - 2
 let laterYears = myAge - 2;
-laterYears = laterYears * 4;
+// laterYears = laterYears * 4;
+laterYears *= 4;
 
 //My age in dog years
 const myAgeInDogYears = earlyYears + laterYears;
 
 //My name in lowercase
-const myName = 'Loraine'.toLowerCase();
+const myName = "Loraine".toLowerCase();
 
 //Answers
 console.log(`earlyYears: ${earlyYears} laterYears: ${laterYears}`);
-console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} in dog years.`);
+console.log(
+  `My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} in dog years.`
+);
