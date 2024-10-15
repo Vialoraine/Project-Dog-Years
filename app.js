fd3d1f9e45cf07485d5c65b7500870d21d9ae0c6
Loreina Salalila <vialoraine.gg@gmail.com>
2024-10-15 01:30:04 -0700

    task 6 completed

--- app.js
+++ app.js
@@ -8,3 +8,6 @@ earlyYears = earlyYears * 10.5;
 //Initializing laterYears to have a value of myAge - 2
 let laterYears = myAge - 2;
 laterYears = laterYears * 4;
+
+//Answers
+console.log(`earlyYears: ${earlyYears} laterYears: ${laterYears}`);
